afa595e0fd1eeb3058cf7f79a22b1b4880b93d11
mictan0910 <mictan0910@highgateschool.org.uk>
2023-03-09 12:04:54 +0000

    updated 1

---
+++ EmailNBC/model.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base, validates
+import re
+
+class Email:
+    ...
+
+
+class Base:
+    ...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
