6646830586760991c419e10274a4a478ba913adc
Michael <mictan0910@highgateschool.org.uk>
2023-03-12 20:05:44 +0000

    UPDATE FROM HOME

--- EmailNBC/model.py
+++ EmailNBC/model.py
@@ -1,37 +1,23 @@
-from sqlalchemy import Column, Integer, String
+import sqlalchemy as db
 from sqlalchemy.orm import declarative_base, validates
 import re
 
-class Email:
-    ...
+Base = declarative_base()
+class EmailAddress(Base):
+    __tablename__ = 'Emails'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+
+    def __repr__(self):
+        return self.email
 
 
-class Base:
-    ...
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@validates('email')
+def validate_email(self, key, address):
+    pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    if not re.fullmatch(pattern, address):
+        raise ValueError('Invalid email address')
+    if key != 'email':
+        raise ValueError('Key must be "email"')
+    return address
 
